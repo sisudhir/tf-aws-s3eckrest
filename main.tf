21b4654a7d41aad3f122f65c773aa5dd9359b1d2
Sindhu Sudhir <sisudhir@cisco.com>
2021-08-24 01:00:27 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,30 @@
+
+terraform {
+  required_providers {
+    restapi = {
+      source = "KirillMeleshko/restapi"
+      version = "1.15.0"
+    }
+  }
+}
+
+
+provider "restapi" {
+  uri                  = "http://192.168.1.93:31677/_snapshot"
+  debug                = true
+  headers              = {"Content-Type" = "application/json"}
+  write_returns_object = true
+  insecure             = true
+  id_attribute         = "/"
+  create_method        = "PUT"
+  update_method        = "PUT"
+  destroy_method       = "PUT"
+}
+
+resource "restapi_object" "create_repository" {
+  object_id = "s3repo"
+  path = "/eck-ss"
+  data = "{\"type\": \"s3\", \"settings\": {\"client\": \"default\", \"bucket\": \"eck-bucket\", \"base_path\": \"eck-ss/\"}}"
+}
+
+
