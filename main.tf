dac911cc7512bba2b66f5d660a0e18901eb02277
Sindhu Sudhir <sisudhir@cisco.com>
2021-08-24 01:27:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,6 +27,18 @@ resource "restapi_object" "create_repository" {
   data = "{\"type\": \"s3\", \"settings\": {\"client\": \"default\", \"bucket\": \"eck-bucket\", \"base_path\": \"eck-ss/\"}}"
 }
 
+provider "restapi" {
+  uri                  = "http://a60156a13057d41a4b0454e5d4a7e0f6-1247086191.us-east-1.elb.amazonaws.com:9200/_snapshot"
+  debug                = true
+  headers              = {"Content-Type" = "application/json"}
+  write_returns_object = true
+  insecure             = true
+  id_attribute         = "/"
+  create_method        = "POST"
+  update_method        = "POST"
+  destroy_method       = "POST"
+}
+
 resource "restapi_object" "restore_repository" {
   object_id = "s3restore"
   path = "/eck-ss/snapshot-2/_restore"
