6b3248a2453cf17ce7233ad73ec0632d703df35b
Sindhu Sudhir <sisudhir@cisco.com>
2021-08-26 19:04:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -27,22 +27,22 @@ resource "restapi_object" "create_repository" {
   data = "{\"type\": \"s3\", \"settings\": {\"client\": \"default\", \"bucket\": \"eck-bucket\", \"base_path\": \"eck-ss/\"}}"
 }
 
-provider "restapi" {
-  alias                = "post-method"
-  uri                  = "http://a60156a13057d41a4b0454e5d4a7e0f6-1247086191.us-east-1.elb.amazonaws.com:9200/_snapshot"
-  debug                = true
-  headers              = {"Content-Type" = "application/json"}
-  write_returns_object = true
-  insecure             = true
-  id_attribute         = "/"
-  create_method        = "POST"
-  update_method        = "POST"
-  destroy_method       = "POST"
-}
+#provider "restapi" {
+  #alias                = "post-method"
+  #uri                  = "http://a60156a13057d41a4b0454e5d4a7e0f6-1247086191.us-east-1.elb.amazonaws.com:9200/_snapshot"
+  #debug                = true
+  #headers              = {"Content-Type" = "application/json"}
+  #write_returns_object = true
+  #insecure             = true
+  #id_attribute         = "/"
+  #create_method        = "POST"
+  #update_method        = "POST"
+  #destroy_method       = "POST"
+#}
 
-resource "restapi_object" "restore_repository" {
-  provider = restapi.post-method
-  object_id = "s3restore"
-  path = "/eck-ss/index-8/_restore"
-  data = ""
-}
+#resource "restapi_object" "restore_repository" {
+  #provider = restapi.post-method
+  #object_id = "s3restore"
+  #path = "/eck-ss/index-8/_restore"
+  #data = ""
+#}
